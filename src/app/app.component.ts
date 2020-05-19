a9d7aa8483ecafe43e62aada03436af30b90ee19
Jacob Neterer <jacobrneterer@gmail.com>
2020-05-19 15:40:46 -0400

    Purging Unused TailwindCSS Styles
    Purging unused TailwindCSS styles reducing style sheet from:
    - development: 12.4 MB -> 104 kB
    - production: 2.4 MB -> 11.6 kB

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { NavigationEnd, Router } from '@angular/router';
+import { NavigationEnd, Router, NavigationStart } from '@angular/router';
 import { filter } from 'rxjs/operators';
 import { ContentService } from './shared/services/content/content.service';
 
@@ -10,17 +10,33 @@ import { ContentService } from './shared/services/content/content.service';
 export class AppComponent implements OnInit {
   previousUrl: string = '';
   currentUrl: string = '';
+  scrollStack: number[] = [];
 
   constructor(private router: Router,
               private contentService: ContentService) { }
 
   ngOnInit(): void {
-    this.router.events.pipe(
-      filter(event =>  event instanceof NavigationEnd)
-    ).subscribe((event: NavigationEnd) => {
-      this.previousUrl = this.currentUrl;
-      this.currentUrl = event.url;
-      this.contentService.setUrlData(this.currentUrl, this.previousUrl);
+    this.router.events.
+    pipe(
+      filter(event => (event instanceof NavigationStart || event instanceof NavigationEnd))
+    )
+    .subscribe((event: NavigationEnd) => {
+      if (event instanceof NavigationStart) {
+        if (event.url !== this.previousUrl) {
+          this.scrollStack.push(window.scrollY);
+        }
+      } else if (event instanceof NavigationEnd) {
+        if (event.url === this.previousUrl) {
+          // Scroll the user to their previous location on the page if they go back.
+          window.scrollTo(0, this.scrollStack.pop());
+        } else {
+          // Scroll the user to the top of the page on navigation.
+          window.scrollTo({ left: 0, top: 0, behavior: 'smooth' });
+        }
+        this.previousUrl = this.currentUrl;
+        this.currentUrl = event.url;
+        this.contentService.setUrlData(this.currentUrl, this.previousUrl);
+      }
     });
   }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,11 @@
 module.exports = {
-  purge: [],
+  purge: {
+    enabled: true,
+    content: [
+      './src/**/*.html',
+      './src/**/*.scss'
+    ]
+  },
   theme: {
     boxShadow: {
       default: '0 1px 3px 0 rgba(0, 0, 0, .1), 0 1px 2px 0 rgba(0, 0, 0, .06)',
